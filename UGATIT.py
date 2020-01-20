ba7db67b62e8b3551c267fece33ec01bf1fd7327
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-01-20 16:32:27 +0800

    update

--- UGATIT.py
+++ UGATIT.py
@@ -491,11 +491,11 @@ class UGATIT(object):
             # G_vars = [var for var in t_vars if 'generator' in var.name]
             # D_vars = [var for var in t_vars if 'discriminator' in var.name]
 
-            with tf.device('/gpu:5'):
+            with tf.device('/gpu:1'):
                 self.G_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Generator_loss,
                                                                                                 var_list=G_vars)
 
-            with tf.device('/gpu:6'):
+            with tf.device('/gpu:2'):
                 self.D_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Discriminator_loss,
                                                                                                 var_list=D_vars)
 
