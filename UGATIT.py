96dd3bd11e34f55a48b8d197a4212be72527cc39
zhengsheng.wcl <zhengsheng.wcl@alibaba-inc.com>
2020-01-20 17:10:49 +0800

    update

--- UGATIT.py
+++ UGATIT.py
@@ -376,94 +376,69 @@ class UGATIT(object):
             self.domain_B = trainB_iterator.get_next()
 
             """ Define Generator, Discriminator """
-            with tf.device('/gpu:3'):
-                x_ab, cam_ab = self.generate_a2b(self.domain_A)  # real a
-                x_ba, cam_ba = self.generate_b2a(self.domain_B)  # real b
-                x_aba, _ = self.generate_b2a(x_ab, reuse=True)  # real b
-                x_bab, _ = self.generate_a2b(x_ba, reuse=True)  # real a
-                x_aa, cam_aa = self.generate_b2a(self.domain_A, reuse=True)  # fake b
-                x_bb, cam_bb = self.generate_a2b(self.domain_B, reuse=True)  # fake a
+            x_ab, cam_ab = self.generate_a2b(self.domain_A)  # real a
+            x_ba, cam_ba = self.generate_b2a(self.domain_B)  # real b
+            x_aba, _ = self.generate_b2a(x_ab, reuse=True)  # real b
+            x_bab, _ = self.generate_a2b(x_ba, reuse=True)  # real a
+            x_aa, cam_aa = self.generate_b2a(self.domain_A, reuse=True)  # fake b
+            x_bb, cam_bb = self.generate_a2b(self.domain_B, reuse=True)  # fake a
 
-            with tf.device('/gpu:4'):
-                real_A_logit, real_A_cam_logit, real_B_logit, real_B_cam_logit = \
-                    self.discriminate_real(self.domain_A, self.domain_B)
-                fake_A_logit, fake_A_cam_logit, fake_B_logit, fake_B_cam_logit = \
-                    self.discriminate_fake(x_ba, x_ab)
+            real_A_logit, real_A_cam_logit, real_B_logit, real_B_cam_logit = \
+                self.discriminate_real(self.domain_A, self.domain_B)
+            fake_A_logit, fake_A_cam_logit, fake_B_logit, fake_B_cam_logit = \
+                self.discriminate_fake(x_ba, x_ab)
 
             """ Define Loss """
-            with tf.device('/gpu:5'):
-                if self.gan_type.__contains__('wgan') or self.gan_type == 'dragan':
-                    GP_A, GP_CAM_A = self.gradient_panalty(real=self.domain_A, fake=x_ba, scope="discriminator_A")
-                    GP_B, GP_CAM_B = self.gradient_panalty(real=self.domain_B, fake=x_ab, scope="discriminator_B")
-                else:
-                    GP_A, GP_CAM_A = 0, 0
-                    GP_B, GP_CAM_B = 0, 0
+            if self.gan_type.__contains__('wgan') or self.gan_type == 'dragan':
+                GP_A, GP_CAM_A = self.gradient_panalty(real=self.domain_A, fake=x_ba, scope="discriminator_A")
+                GP_B, GP_CAM_B = self.gradient_panalty(real=self.domain_B, fake=x_ab, scope="discriminator_B")
+            else:
+                GP_A, GP_CAM_A = 0, 0
+                GP_B, GP_CAM_B = 0, 0
 
-            with tf.device('/gpu:1'):
-                G_ad_loss_A = (
-                        generator_loss(self.gan_type, fake_A_logit) + generator_loss(self.gan_type, fake_A_cam_logit))
-            with tf.device('/gpu:2'):
-                G_ad_loss_B = (
-                        generator_loss(self.gan_type, fake_B_logit) + generator_loss(self.gan_type, fake_B_cam_logit))
+            G_ad_loss_A = (
+                    generator_loss(self.gan_type, fake_A_logit) + generator_loss(self.gan_type, fake_A_cam_logit))
+            G_ad_loss_B = (
+                    generator_loss(self.gan_type, fake_B_logit) + generator_loss(self.gan_type, fake_B_cam_logit))
 
-            with tf.device('/gpu:1'):
-                D_ad_loss_A = (discriminator_loss(self.gan_type, real_A_logit, fake_A_logit) + discriminator_loss(
-                    self.gan_type, real_A_cam_logit, fake_A_cam_logit) + GP_A + GP_CAM_A)
-            with tf.device('/gpu:2'):
-                D_ad_loss_B = (discriminator_loss(self.gan_type, real_B_logit, fake_B_logit) + discriminator_loss(
-                    self.gan_type, real_B_cam_logit, fake_B_cam_logit) + GP_B + GP_CAM_B)
+            D_ad_loss_A = (discriminator_loss(self.gan_type, real_A_logit, fake_A_logit) + discriminator_loss(
+                self.gan_type, real_A_cam_logit, fake_A_cam_logit) + GP_A + GP_CAM_A)
+            D_ad_loss_B = (discriminator_loss(self.gan_type, real_B_logit, fake_B_logit) + discriminator_loss(
+                self.gan_type, real_B_cam_logit, fake_B_cam_logit) + GP_B + GP_CAM_B)
 
-            with tf.device('/gpu:3'):
-                reconstruction_A = L1_loss(x_aba, self.domain_A)  # reconstruction
-                reconstruction_B = L1_loss(x_bab, self.domain_B)  # reconstruction
+            reconstruction_A = L1_loss(x_aba, self.domain_A)  # reconstruction
+            reconstruction_B = L1_loss(x_bab, self.domain_B)  # reconstruction
 
-            with tf.device('/gpu:4'):
-                identity_A = L1_loss(x_aa, self.domain_A)
-                identity_B = L1_loss(x_bb, self.domain_B)
+            identity_A = L1_loss(x_aa, self.domain_A)
+            identity_B = L1_loss(x_bb, self.domain_B)
 
-            with tf.device('/gpu:1'):
-                cam_A = cam_loss(source=cam_ba, non_source=cam_aa)
-                cam_B = cam_loss(source=cam_ab, non_source=cam_bb)
+            cam_A = cam_loss(source=cam_ba, non_source=cam_aa)
+            cam_B = cam_loss(source=cam_ab, non_source=cam_bb)
 
-            with tf.device('/gpu:2'):
-                Generator_A_gan = self.adv_weight * G_ad_loss_A
-                Generator_A_cycle = self.cycle_weight * reconstruction_B
-                Generator_A_identity = self.identity_weight * identity_A
-                Generator_A_cam = self.cam_weight * cam_A
+            Generator_A_gan = self.adv_weight * G_ad_loss_A
+            Generator_A_cycle = self.cycle_weight * reconstruction_B
+            Generator_A_identity = self.identity_weight * identity_A
+            Generator_A_cam = self.cam_weight * cam_A
 
-            with tf.device('/gpu:3'):
-                Generator_B_gan = self.adv_weight * G_ad_loss_B
-                Generator_B_cycle = self.cycle_weight * reconstruction_A
-                Generator_B_identity = self.identity_weight * identity_B
-                Generator_B_cam = self.cam_weight * cam_B
+            Generator_B_gan = self.adv_weight * G_ad_loss_B
+            Generator_B_cycle = self.cycle_weight * reconstruction_A
+            Generator_B_identity = self.identity_weight * identity_B
+            Generator_B_cam = self.cam_weight * cam_B
 
-            with tf.device('/gpu:4'):
-                Generator_A_loss = Generator_A_gan + Generator_A_cycle + Generator_A_identity + Generator_A_cam
-                Generator_B_loss = Generator_B_gan + Generator_B_cycle + Generator_B_identity + Generator_B_cam
+            Generator_A_loss = Generator_A_gan + Generator_A_cycle + Generator_A_identity + Generator_A_cam
+            Generator_B_loss = Generator_B_gan + Generator_B_cycle + Generator_B_identity + Generator_B_cam
 
-            with tf.device('/gpu:1'):
-                Discriminator_A_loss = self.adv_weight * D_ad_loss_A
-                Discriminator_B_loss = self.adv_weight * D_ad_loss_B
+            Discriminator_A_loss = self.adv_weight * D_ad_loss_A
+            Discriminator_B_loss = self.adv_weight * D_ad_loss_B
 
-            with tf.device('/gpu:2'):
-                self.Generator_loss = Generator_A_loss + Generator_B_loss + regularization_loss('generator')
-
-            with tf.device('/gpu:3'):
-                self.Discriminator_loss = Discriminator_A_loss + Discriminator_B_loss + regularization_loss(
-                    'discriminator')
+            self.Generator_loss = Generator_A_loss + Generator_B_loss + regularization_loss('generator')
+            self.Discriminator_loss = Discriminator_A_loss + Discriminator_B_loss + regularization_loss('discriminator')
 
             """ Result Image """
-            with tf.device('/gpu:1'):
-                self.fake_A = x_ba
-
-            with tf.device('/gpu:2'):
-                self.fake_B = x_ab
-
-            with tf.device('/gpu:3'):
-                self.real_A = self.domain_A
-
-            with tf.device('/gpu:4'):
-                self.real_B = self.domain_B
+            self.fake_A = x_ba
+            self.fake_B = x_ab
+            self.real_A = self.domain_A
+            self.real_B = self.domain_B
 
             """ Training """
             devices = self.get_available_gpus()
@@ -484,36 +459,28 @@ class UGATIT(object):
             # G_vars = [var for var in t_vars if 'generator' in var.name]
             # D_vars = [var for var in t_vars if 'discriminator' in var.name]
 
-            with tf.device('/gpu:1'):
-                self.G_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Generator_loss,
-                                                                                                var_list=G_vars)
-
-            with tf.device('/gpu:2'):
-                self.D_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Discriminator_loss,
-                                                                                                var_list=D_vars)
+            self.G_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Generator_loss,
+                                                                                            var_list=G_vars)
+            self.D_optim = tf.train.AdamOptimizer(self.lr, beta1=0.5, beta2=0.999).minimize(self.Discriminator_loss,
+                                                                                            var_list=D_vars)
 
             """" Summary """
-            with tf.device('/gpu:6'):
-                self.all_G_loss = tf.summary.scalar("Generator_loss", self.Generator_loss)
-                self.all_D_loss = tf.summary.scalar("Discriminator_loss", self.Discriminator_loss)
+            self.all_G_loss = tf.summary.scalar("Generator_loss", self.Generator_loss)
+            self.all_D_loss = tf.summary.scalar("Discriminator_loss", self.Discriminator_loss)
 
-            with tf.device('/gpu:2'):
-                self.G_A_loss = tf.summary.scalar("G_A_loss", Generator_A_loss)
-                self.G_A_gan = tf.summary.scalar("G_A_gan", Generator_A_gan)
-                self.G_A_cycle = tf.summary.scalar("G_A_cycle", Generator_A_cycle)
-                self.G_A_identity = tf.summary.scalar("G_A_identity", Generator_A_identity)
-                self.G_A_cam = tf.summary.scalar("G_A_cam", Generator_A_cam)
+            self.G_A_loss = tf.summary.scalar("G_A_loss", Generator_A_loss)
+            self.G_A_gan = tf.summary.scalar("G_A_gan", Generator_A_gan)
+            self.G_A_cycle = tf.summary.scalar("G_A_cycle", Generator_A_cycle)
+            self.G_A_identity = tf.summary.scalar("G_A_identity", Generator_A_identity)
+            self.G_A_cam = tf.summary.scalar("G_A_cam", Generator_A_cam)
 
-            with tf.device('/gpu:3'):
-                self.G_B_loss = tf.summary.scalar("G_B_loss", Generator_B_loss)
-                self.G_B_gan = tf.summary.scalar("G_B_gan", Generator_B_gan)
-                self.G_B_cycle = tf.summary.scalar("G_B_cycle", Generator_B_cycle)
-                self.G_B_identity = tf.summary.scalar("G_B_identity", Generator_B_identity)
-                self.G_B_cam = tf.summary.scalar("G_B_cam", Generator_B_cam)
-
-            with tf.device('/gpu:4'):
-                self.D_A_loss = tf.summary.scalar("D_A_loss", Discriminator_A_loss)
-                self.D_B_loss = tf.summary.scalar("D_B_loss", Discriminator_B_loss)
+            self.G_B_loss = tf.summary.scalar("G_B_loss", Generator_B_loss)
+            self.G_B_gan = tf.summary.scalar("G_B_gan", Generator_B_gan)
+            self.G_B_cycle = tf.summary.scalar("G_B_cycle", Generator_B_cycle)
+            self.G_B_identity = tf.summary.scalar("G_B_identity", Generator_B_identity)
+            self.G_B_cam = tf.summary.scalar("G_B_cam", Generator_B_cam)
+            self.D_A_loss = tf.summary.scalar("D_A_loss", Discriminator_A_loss)
+            self.D_B_loss = tf.summary.scalar("D_B_loss", Discriminator_B_loss)
 
             self.rho_var = []
             for var in tf.trainable_variables():
@@ -530,10 +497,8 @@ class UGATIT(object):
             g_summary_list.extend(self.rho_var)
             d_summary_list = [self.D_A_loss, self.D_B_loss, self.all_D_loss]
 
-            with tf.device('/gpu:3'):
-                self.G_loss = tf.summary.merge(g_summary_list)
-            with tf.device('/gpu:4'):
-                self.D_loss = tf.summary.merge(d_summary_list)
+            self.G_loss = tf.summary.merge(g_summary_list)
+            self.D_loss = tf.summary.merge(d_summary_list)
 
         else:
             """ Test """
